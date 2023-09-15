4ea2a17d10c90f51b22d7490754d2b163995c248
luizPrendin <luizprendinfelipe@gmail.com>
2023-09-14 22:44:00 -0300

    add conteudo ao componente Produtos

---
+++ src/routes/Produtos/index.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom"
+import { listaProdutos } from "../../components/ListaProdutos"
+
+export default function Produtos(){
+
+    return(
+        <main>
+            <h1>Produtos</h1>
+            {listaProdutos.map(prod =>
+                (
+                  <div key={prod.id}>
+                    <Link to={`/produtos/editar/${prod.id}`}>
+                        Editar o produto: {prod.nome}
+                    </Link>
+                  </div>  
+                ))}
+        </main>
+    )
+}
